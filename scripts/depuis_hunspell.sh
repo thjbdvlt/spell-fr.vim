053615e1b83d5f2b144c17a4fa78a67e09ff8c79
thjbbvlt <thibault2@ik.me>
2024-05-07 11:39:23 +0200

    réunis scripts

--- scripts/depuis_hunspell.sh
+++ scripts/depuis_hunspell.sh
@@ -76,10 +76,86 @@ sed -E -i 's/^(SFX \S+ \S+ \S+ \S+) (.*$)/\1 # \2/' $aff
 # s'il manque le '/' et qu'ensuite il y a un commentaire, alors la ligne est discard. il faut donc les ajouter.
 sed -E -i 's|^(\w+) |\1/ |' $dic
 
-## todo: ici, faire un seul script, plutôt. parce qu'ils sont toujours utilisés ensemble et dans le même ordre: pas de sens à ce qu'ils soint séparés.
-s=$(git rev-parse --show-toplevel)/scripts
-# utiliser le script qui modifie les affixes dans le fichier .dic (essentiellement pour les suffixes féminin/masculin et l'écriture inclusive).
-${s}/remplacer-sfx.sh
-# utiliser les deux scripts qui remplacent les part-of-speech (où les autres informations qui sont placées dans l'attributs `po:`) par des POS tags standardisés (upos).
-${s}/vers-upos.sh $dic
-${s}/po-to-is-aff.sh $aff
+### modifications des flags du dic, accordément aux modifications que j'écris (manuellement) dans la définitions des affixes (essentiellement pour une meilleure prise en compte de l'écriture inclusive).
+# auteur·rice
+sed -E -i "s;eur/(Fc|Gc|F\.Gc);eur·rice/ri;" $dic
+# vif·ive, sauf·ve
+sed -E -i "s|([iïu])f/F\.|\1f·ve/fv|" $dic
+# aigu·ue
+sed -E -i "s|gu/Fx|gu·ue/ue|" $dic
+# accordeur·euse
+sed -E -i "s|eur/Fs|eur·euse/rs|" $dic
+# poète·sse, prophète·sse
+sed -E -i "s|ète/F\.|ète·sse/ee|" $dic
+# bref·ève
+sed -E -i "s|ef/F\.|ef·ève/ev|" $dic
+# forestier·ère
+sed -E -i "s|er/F\.|er·ère/rr|" $dic
+# public·que
+sed -E -i "s|c/F\.|c·que/qc|" $dic
+# incomplet·ète
+sed -E -i "s|et/F\.|et·ète/et|" $dic
+# ...en·ène
+sed -E -i "s|en/F\.|en·ène/en|" $dic
+# cramberry, berries
+sed -E -i "s;(y|sh|man|x)/A\.;\1/GB;" $dic
+# une exception avant la suite: sec·èche
+sed -E -i "s,^sec/.*,sec·èche/ec," $dic
+# long·ue
+sed -E -i "s|g/F\.|g·ue/gu|" $dic
+# les autres féminin avec le flag "F." deviennent la forme avec un "e" à la fin: enseignant·e, zurichois·e, vrai·e. important: ne pas faire avant les traitement spéciaux comme "forestier·ère".
+sed -E -i "s@/F\.@·e/_e@" $dic
+# les féminins qui double la dernière consonne
+sed -E -i "s@([flnst])/F\+@\1·\1e/ll@" $dic
+# les féminins qui ajoute -sse (prêtre·sse)
+sed -E -i "s@e/F\+@e·sse/ss@" $dic
+# fou·olle
+sed -E -i "s@([mf]o)u/F\+@\1u·olle/lu@" $dic
+# frauduleux·euse
+sed -E -i "s|([eo]u)x/W\.|\1x·euse/xz|" $dic
+# global·e
+sed -E -i "s|al/W\.|al·e/al|" $dic
+# tourtereau·elle
+sed -E -i "s|eau/W\.|eau·elle/la|" $dic
+# beau
+sed -E -i "s|eau/Wx|eau·elle/la|" $dic
+# doux·ce
+sed -E -i "s|^doux/Wx|doux·ce/xc|" $dic
+# roux·sse
+sed -E -i "s@^(dou|fau)x/Wx@\1x·sse/xs@" $dic
+# vieux·eille
+sed -E -i "s|^vieux/Wx|vieux·eille/xl|" $dic
+# taximan
+sed -E -i "s|man/A\.|man/GB|" $dic
+# flag "Gs": les féminin en ·e sur un mot en ·eur. j'enlève car c'est similaire à des suffixes déjà définis.
+sed -E -i "s|Gs||" $dic
+# renommer quelques suffixes pour adopter une autre terminologie.
+sed -i -E 's/S\./_s/g' $dic
+sed -i -E 's/X\./_x/g' $dic
+sed -i -E 's/I\./LT/g' $dic
+
+# modifie les attributs `po` (part-of-speech) dans le fichier .dic pour les faire correspondre au standard upos.
+# (un seul truc qui ne change pas: po:adj)
+sed -i -E -e 's/po:loc\./po:/g' \
+    -e 's/po:nom\S*/po:noun/g' \
+    -e 's/po:ponc/po:punct/g' \
+    -e 's/po:pro\S*/po:pron/g' \
+    -e 's/po:(sg|epi|mg)//g' \
+    -e 's/po:nb/po:num/g' \
+    -e 's/po:(cjsub|cj)\S*/po:sconj/g' \
+    -e 's/po:(cjco|cj)\S*/po:cconj/g' \
+    -e 's/po:det\S*/po:det/g' \
+    -e 's/po:adv\S*/po:adv/g' \
+    -e 's/po:v\S*/po:verb/g' \
+    -e 's/po:inf\S*//g' \
+    -e 's/po:interj/po:intj/g' \
+    -e 's/po:patr\S*/po:part/g' \
+    -e 's/po:pfx\S*/po:part/g' \
+    -e 's/po:preverb/po:adp/g' \
+    -e 's/po:(\d\S*)/is:\1/g' \
+    -e 's/po:titr/po:pro/g' \
+    -e 's/po:sign//g' \
+    $dic
+
+# modifie les attribut `po` dans le fichier .aff, comme il s'agit essentiellement d'indications de temps verbaux et de personnes, je déplace en `is` (inflectionnal suffix), car il me semble que cela ne relève pas du part-of-speech.
+sed -i -E -e 's/po:/is:/g' $aff
